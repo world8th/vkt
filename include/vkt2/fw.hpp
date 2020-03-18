e3caa20aec886367c8cec3ed7b0da67c7bb8567b
helixd-2k18 <helixd.2k18@gmail.com>
2020-03-18 16:34:56 +0800

    re-sort extensions, add some

--- include/vkt2/fw.hpp
+++ include/vkt2/fw.hpp
@@ -55,72 +55,90 @@ namespace vkt
             "VK_EXT_sample_locations",
             "VK_EXT_conservative_rasterization",
             "VK_EXT_hdr_metadata",
-            "VK_EXT_subgroup_size_control",
-            "VK_EXT_sampler_filter_minmax",
-            "VK_EXT_descriptor_indexing",
-            "VK_EXT_scalar_block_layout",
             
-            "VK_AMD_gpu_shader_int16",
-            "VK_AMD_gpu_shader_half_float",
+            // 
+            "VK_EXT_descriptor_indexing",
+            "VK_KHR_descriptor_update_template",
+            "VK_KHR_push_descriptor",
+
+            // 
             "VK_AMD_gcn_shader",
-            "VK_AMD_buffer_marker",
             "VK_AMD_shader_info",
             "VK_AMD_texture_gather_bias_lod",
             "VK_AMD_shader_image_load_store_lod",
             "VK_AMD_shader_trinary_minmax",
             "VK_AMD_draw_indirect_count",
 
-            "VK_KHR_16bit_storage",
-            "VK_KHR_8bit_storage",
-            "VK_KHR_incremental_present",
-            "VK_KHR_push_descriptor",
-            "VK_KHR_swapchain",
-            "VK_KHR_surface",
-            "VK_KHR_display",
+            // 
+            "VK_EXT_sampler_filter_minmax",
             "VK_KHR_sampler_ycbcr_conversion",
-            "VK_KHR_image_format_list",
-            "VK_KHR_shader_draw_parameters",
-            "VK_KHR_variable_pointers",
-            "VK_KHR_dedicated_allocation",
-            "VK_KHR_relaxed_block_layout",
-            "VK_KHR_descriptor_update_template",
             "VK_KHR_sampler_mirror_clamp_to_edge",
-            "VK_KHR_storage_buffer_storage_class",
-            "VK_KHR_vulkan_memory_model",
-            "VK_KHR_dedicated_allocation",
-            "VK_KHR_driver_properties",
-            "VK_KHR_get_memory_requirements2",
+
+            // 
             "VK_KHR_bind_memory2",
             "VK_KHR_maintenance1",
             "VK_KHR_maintenance2",
             "VK_KHR_maintenance3",
+            "VK_KHR_driver_properties",
+            "VK_KHR_image_format_list",
+            "VK_KHR_dedicated_allocation",
+            "VK_KHR_imageless_framebuffer",
+            "VK_KHR_shader_draw_parameters",
+            "VK_KHR_get_memory_requirements2",
+            
+            // 
+            "VK_EXT_scalar_block_layout",
+            "VK_KHR_storage_buffer_storage_class",
+            "VK_KHR_relaxed_block_layout",
+            "VK_KHR_variable_pointers",
+            "VK_AMD_buffer_marker",
+
+            // 
+            "VK_EXT_subgroup_size_control",
+            "VK_KHR_shader_subgroup_extended_types",
+
+            // 
+            "VK_KHR_16bit_storage",
+            "VK_KHR_8bit_storage",
+            "VK_AMD_gpu_shader_int16",
+            "VK_AMD_gpu_shader_half_float",
             "VK_KHX_shader_explicit_arithmetic_types",
             "VK_KHR_shader_atomic_int64",
+
+            // 
+            "VK_KHR_incremental_present",
+            "VK_KHR_ray_tracing",
+            "VK_KHR_pipeline_library",
+            "VK_KHR_deferred_host_operations",
+            "VK_KHR_buffer_device_address",
+            "VK_KHR_vulkan_memory_model",
             "VK_KHR_shader_float16_int8",
             "VK_KHR_shader_float_controls",
             "VK_KHR_shader_clock",
-            "VK_KHR_imageless_framebuffer",
-            "VK_KHR_storage_buffer_storage_class",
-            "VK_KHR_shader_subgroup_extended_types",
-            "VK_KHR_ray_tracing",
+            "VK_KHR_swapchain",
+            "VK_KHR_surface",
+            "VK_KHR_display",
 
+            // 
             "VK_NV_compute_shader_derivatives",
             "VK_NV_corner_sampled_image",
             "VK_NV_shader_image_footprint",
             "VK_NV_shader_subgroup_partitioned",
             "VK_NV_shader_sm_builtins",
+            "VK_NV_ray_tracing",
 
+            // 
             "VK_KHR_external_memory",
             "VK_KHR_external_memory_capabilities",
             "VK_KHR_external_memory_win32",
 
+            // 
             "VK_KHR_external_semaphore",
             "VK_KHR_external_semaphore_capabilities",
             "VK_KHR_external_semaphore_win32",
 
-            "VK_NV_ray_tracing",
+            // 
             "VK_NVX_image_view_handle",
-
             "VK_EXT_validation_cache",
             "VK_EXT_validation_features",
             "VK_EXT_validation_flags", 
