70340dcb0b9a7561f792fc2a6e5cb75d9dcaa46b
helixd-2k18 <helixd.2k18@gmail.com>
2020-05-26 20:11:59 +0800

    update for swap arguments

--- include/vkt3/essential.hpp
+++ include/vkt3/essential.hpp
@@ -442,20 +442,7 @@ namespace vkt {
     };
 #endif
 
-    // 
-    struct VmaMemoryInfo {
-        VmaMemoryUsage memUsage = VMA_MEMORY_USAGE_GPU_ONLY;
-        vkt::uni_ptr<xvk::Device> deviceDispatch = {};
-        vkt::uni_ptr<xvk::Instance> instanceDispatch = {};
 
-        // Resolve JavaCPP Problem...
-        VmaMemoryInfo& setDeviceDispatch(std::shared_ptr<xvk::Device> ptr) { deviceDispatch = ptr; return *this; };
-        VmaMemoryInfo& setInstanceDispatch(std::shared_ptr<xvk::Instance> ptr) { instanceDispatch = ptr; return *this; };
-
-        // Resolve JavaCPP Problem...
-        std::shared_ptr<xvk::Device>& getDeviceDispatch() { return deviceDispatch; };
-        std::shared_ptr<xvk::Instance>& getInstanceDispatch() { return instanceDispatch; };
-    };
 
 
 
@@ -488,6 +475,21 @@ namespace vkt {
             };
         };
     #endif
+    };
+
+    // 
+    struct VmaMemoryInfo {
+        VmaMemoryUsage memUsage = VMA_MEMORY_USAGE_GPU_ONLY;
+        vkt::uni_ptr<xvk::Instance> instanceDispatch = vkGlobal::instance;
+        vkt::uni_ptr<xvk::Device> deviceDispatch = vkGlobal::device;
+
+        // Resolve JavaCPP Problem...
+        VmaMemoryInfo& setDeviceDispatch(std::shared_ptr<xvk::Device> ptr) { deviceDispatch = ptr; return *this; };
+        VmaMemoryInfo& setInstanceDispatch(std::shared_ptr<xvk::Instance> ptr) { instanceDispatch = ptr; return *this; };
+
+        // Resolve JavaCPP Problem...
+        std::shared_ptr<xvk::Device>& getDeviceDispatch() { return deviceDispatch; };
+        std::shared_ptr<xvk::Instance>& getInstanceDispatch() { return instanceDispatch; };
     };
 
     // TODO: Add XVK support
